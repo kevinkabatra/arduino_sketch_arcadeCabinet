f3fc1e018edea3aebd9fab04cea0f31101aa037f
Kevin Kabatra <kevin@lsretail.com>
2019-01-06 18:07:28 +0000

    Refactored to use one interrupt
    I have had lots of issues on the hardware side of this project to use all of the built in interrupts on the Leonardo. Using pins 0 and 1 have caused issues where a lot of false interrupts were being triggered. I think that could be caused by the fact that the 0 and 1 pins are being used for the serial communication. Realized that I could simplify the software by having all of the hardware buttons trigger the same interrupt pin, 3 in the code now, and then each button could maintain its position, pressed or released, with digital pins assigned directly to the buttons, pins 4 - 7 in the code.

--- sketch_arcadeCabinet.ino
+++ sketch_arcadeCabinet.ino
@@ -2,6 +2,9 @@
 #include <LED.h>
 #include <Keyboard.h>
 
+//Set interrupt pin
+byte interruptPin = 3;
+
 // Construct the LEDs
 const short numberOfLEDs = 4;
 LED leds[numberOfLEDs] = {
@@ -11,26 +14,19 @@ LED leds[numberOfLEDs] = {
   LED("Player 2 Coin" , 11, true)  
 };
 
-//Setup One intterrupt, all four buttons will trigger this code
-//All four buttons will need to run throught a schmitt trigger and also run directly to the Arduino
-//Check each button for its current state
-
 //Define keyboard buttons, find more keycodes here: http://keycode.info/
 byte numberOneASCII   = 49;
 byte numberTwoASCII   = 50;
 byte numberThreeASCII = 51;
 byte numberFourASCII  = 52;
 
-//Set interrupt in
-byte interruptPin = 3;
-
 // Construct the arcade buttons
 const short numberOfArcadeButtons = 4;
 ArcadeButton arcadeButtons[numberOfArcadeButtons] = {
-  ArcadeButton("Player 1 Start", 7 , numberOneASCII  ),
-  ArcadeButton("Player 2 Start", 8 , numberTwoASCII  ),
-  ArcadeButton("Player 1 Coin" , 9 , numberThreeASCII),
-  ArcadeButton("Player 2 Coin" , 10, numberFourASCII )
+  ArcadeButton("Player 1 Start", 4, numberOneASCII  ),
+  ArcadeButton("Player 2 Start", 5, numberTwoASCII  ),
+  ArcadeButton("Player 1 Coin" , 6, numberThreeASCII),
+  ArcadeButton("Player 2 Coin" , 7, numberFourASCII )
 };
 
 /**
@@ -38,10 +34,7 @@ ArcadeButton arcadeButtons[numberOfArcadeButtons] = {
  */
 void setup() {
   // Setup the button interrupts
-  setupPlayer1StartInterrupt();
-  setupPlayer2StartInterrupt();
-  setupPlayer1CoinInterrupt();
-  setupPlayer2CoinInterrupt(); 
+  setupArcadeButtonsInterrupt();
 }
 
 /**
@@ -49,48 +42,42 @@ void setup() {
  * that is completed within the function and the
  * onboard crystal oscillator.
  */
-void loop() {  
+void loop() {
 }
 
-/**
- * The interrupt for Player 1 Start button.
+/*
+ * Sets up the interrupt handler to listen for button presses.
  * 
- * This will be fired each time the button
- * changes state. The button is connected to
- * a RC network and a Schmitt Trigger so the
- * debouncing will be handled by the hardware.
+ * The interrupt is setup for the Players 1 and 2 Start and
+ * Coin buttons. The interrupt will be fired each time the 
+ * button changes state. The button is connected to a RC 
+ * network and a Schmitt Trigger so the debouncing will be 
+ * handled by the hardware.
+ * 
+ * The Arduino Leonardo does not easily support using the 
+ * five built-in digital pin interrupts, I have noticed a
+ * lot of noise on the 0 and 1 digital pins. Due to this
+ * I have the buttons triggering the same interrupt and
+ * allow the handler to determine which button is currently
+ * being pressed.
  */
-void setupPlayer1StartInterrupt() {
+void setupArcadeButtonsInterrupt() {
   attachInterrupt(
-    digitalPinToInterrupt(arcadeButtons[0].GetDigitalPin()),
-    []() {
+    digitalPinToInterrupt(interruptPin),
+    [](){
+      //Player 1 Start
       if(digitalRead(arcadeButtons[0].GetDigitalPin())) {
         leds[0].SetOff();
         Keyboard.begin();
-        Keyboard.press(arcadeButtons[0].GetASCIIKeyCode());            
+        Keyboard.press(arcadeButtons[0].GetASCIIKeyCode());
       }
       else {
         leds[0].SetOn();
         Keyboard.release(arcadeButtons[0].GetASCIIKeyCode());
         Keyboard.end();
       }
-    },
-    CHANGE
-  );
-}
 
-/**
- * The interrupt for Player 2 Start button.
- * 
- * This will be fired each time the button
- * changes state. The button is connected to
- * a RC network and a Schmitt Trigger so the
- * debouncing will be handled by the hardware.
- */
-void setupPlayer2StartInterrupt() {
-  attachInterrupt(
-    digitalPinToInterrupt(arcadeButtons[1].GetDigitalPin()),
-    []() {
+      //Player 2 Start
       if(digitalRead(arcadeButtons[1].GetDigitalPin())) {
         leds[1].SetOff();
         Keyboard.begin();
@@ -101,62 +88,26 @@ void setupPlayer2StartInterrupt() {
         Keyboard.release(arcadeButtons[1].GetASCIIKeyCode());
         Keyboard.end();
       }
-    },
-    CHANGE
-  );
-}
 
-/**
- * The interrupt for Player 1 Coin button.
- * 
- * This will be fired each time the button
- * changes state. The button is connected to
- * a RC network and a Schmitt Trigger so the
- * debouncing will be handled by the hardware.
- * 
- */
-void setupPlayer1CoinInterrupt() {
-  attachInterrupt(
-    digitalPinToInterrupt(arcadeButtons[2].GetDigitalPin()),
-    []() {
+      //Player 1 Coin
       if(digitalRead(arcadeButtons[2].GetDigitalPin())) {
-        leds[2].SetOff();
         Keyboard.begin();
         Keyboard.press(arcadeButtons[2].GetASCIIKeyCode());
       }
       else {
-        leds[2].SetOn();
         Keyboard.release(arcadeButtons[2].GetASCIIKeyCode());
         Keyboard.end();
       }
-    },
-    CHANGE
-  );
-}
 
-/**
- * The interrupt for Player 2 Coin button.
- * 
- * This will be fired each time the button
- * changes state. The button is connected to
- * a RC network and a Schmitt Trigger so the
- * debouncing will be handled by the hardware.
- * 
- */
-void setupPlayer2CoinInterrupt() {
-  attachInterrupt(
-    digitalPinToInterrupt(arcadeButtons[3].GetDigitalPin()),
-    []() {       
+      //Player 2 Coin
       if(digitalRead(arcadeButtons[3].GetDigitalPin())) {
-        leds[3].SetOff();
         Keyboard.begin();
         Keyboard.press(arcadeButtons[3].GetASCIIKeyCode());
       }
       else {
-        leds[3].SetOn();
         Keyboard.release(arcadeButtons[3].GetASCIIKeyCode());
         Keyboard.end();
-      }        
+      }
     },
     CHANGE
   );
